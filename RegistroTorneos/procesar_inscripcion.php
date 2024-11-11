c3a2686206378563a3f38087442239ecfb12b18e
Joel <alanjoelt43@gmail.com>
2024-11-11 09:58:49 +0100

    add

--- RegistroTorneos/procesar_inscripcion.php
+++ RegistroTorneos/procesar_inscripcion.php
@@ -1,13 +1,11 @@
 <?php
-if (isset($_POST['tournamentName'], $_POST['participantName'], $_POST['participantNameDiscord'], $_POST['participantNick'], $_POST['registrationType'], $_POST['playerEmail'], $_POST['playerPhone'], $_POST['id_torneo'])) {
-    // Mostrar cada dato enviado
-    echo "Nombre del Torneo: " . htmlspecialchars($_POST['tournamentName']) . "<br>";
-    echo "Nombre del Participante: " . htmlspecialchars($_POST['participantName']) . "<br>";
-    echo "Nombre en Discord del Participante: " . htmlspecialchars($_POST['participantNameDiscord']) . "<br>";
-    echo "Nick del Participante: " . htmlspecialchars($_POST['participantNick']) . "<br>";
-    echo "Tipo de Inscripción: " . htmlspecialchars($_POST['registrationType']) . "<br>";
-    echo "Correo Electrónico: " . htmlspecialchars($_POST['playerEmail']) . "<br>";
-    echo "Teléfono: " . htmlspecialchars($_POST['playerPhone']) . "<br>";
-    echo "ID del Torneo: " . htmlspecialchars($_POST['id_torneo']) . "<br>";
-}
+// Mostrar cada dato enviado con valores por defecto si faltan algunos campos
+echo "Nombre del Torneo: " . htmlspecialchars($_POST['tournamentName'] ?? 'No proporcionado') . "<br>";
+echo "Nombre del Participante: " . htmlspecialchars($_POST['participantName'] ?? 'No proporcionado') . "<br>";
+echo "Nombre en Discord del Participante: " . htmlspecialchars($_POST['participantNameDiscord'] ?? 'No proporcionado') . "<br>";
+echo "Nick del Participante: " . htmlspecialchars($_POST['participantNick'] ?? 'No proporcionado') . "<br>";
+echo "Tipo de Inscripción: " . htmlspecialchars($_POST['registrationType'] ?? 'No proporcionado') . "<br>";
+echo "Correo Electrónico: " . htmlspecialchars($_POST['playerEmail'] ?? 'No proporcionado') . "<br>";
+echo "Teléfono: " . htmlspecialchars($_POST['playerPhone'] ?? 'No proporcionado') . "<br>";
+echo "ID del Torneo: " . htmlspecialchars($_POST['id_torneo'] ?? 'No proporcionado') . "<br>";
 ?>
